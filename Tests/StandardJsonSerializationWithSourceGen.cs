e386f88720ded7d57bc4ff5b3a3299423436ddac
Christian Daley <christiandaley@gmail.com>
2025-07-20 15:55:28 -0700

    add tests for generic type json source generation

--- Tests/StandardJsonSerializationWithSourceGen.cs
+++ Tests/StandardJsonSerializationWithSourceGen.cs
@@ -7,6 +7,11 @@ using System.Text.Json.Serialization;
 
 [JsonSerializable(typeof(StandardJsonSerializationWithSourceGen.NonGenericType))]
 [JsonSerializable(typeof(StandardJsonSerializationWithSourceGen.NonGenericType.NestedRecord1))]
+[JsonSerializable(typeof(StandardJsonSerializationWithSourceGen.GenericType<string, double, int>))]
+[JsonSerializable(typeof(StandardJsonSerializationWithSourceGen.GenericType<int, Dictionary<string, string>, double>))]
+[JsonSerializable(typeof(StandardJsonSerializationWithSourceGen.GenericType<double, Dictionary<int, int>, Dictionary<string, string>>))]
+[JsonSerializable(typeof(double[]))]
+[JsonSerializable(typeof(Dictionary<int, int>[]))]
 internal partial class StandardJsonSerializationWithSourceGenJsonContext : JsonSerializerContext
 {
 
@@ -27,6 +32,24 @@ public partial class StandardJsonSerializationWithSourceGen
         }
 
         public partial class StandardJsonConverter : JsonConverter<NonGenericType>
+        {
+
+        }
+    }
+
+    [Case("Case0", "T")]
+    [Case("Case1", "U[]")]
+    [Case("Case2", "GenericType<V, Dictionary<T, T>, U>")]
+    [EnableJsonSerialization(UsingSourceGeneration: true)]
+    [JsonConverter(typeof(GenericType.StandardJsonConverter))]
+    internal partial class GenericType<T, U, V>
+    {
+
+    }
+
+    internal static partial class GenericType
+    {
+        public partial class StandardJsonConverter : JsonConverterFactory
         {
 
         }
@@ -104,5 +127,91 @@ public partial class StandardJsonSerializationWithSourceGen
         var deserializedValue = JsonSerializer.Deserialize(json, StandardJsonSerializationWithSourceGenJsonContext.Default.NonGenericType);
 
         Assert.Equal(value, deserializedValue);
+    }
+
+    [Fact]
+    public void GenericTypeCase0()
+    {
+        var value = GenericType<string, double, int>.Case0("abc");
+
+        var json = JsonSerializer.Serialize(value, StandardJsonSerializationWithSourceGenJsonContext.Default.GenericTypeStringDoubleInt32);
+
+        Assert.True(JsonNode.DeepEquals(JsonNode.Parse(json), JsonNode.Parse(@"
+        {
+            ""0"": ""abc""
+        }")));
+
+        var deserializedValue = JsonSerializer.Deserialize(json, StandardJsonSerializationWithSourceGenJsonContext.Default.GenericTypeStringDoubleInt32);
+
+        Assert.Equal(value, deserializedValue);
+    }
+
+    [Fact]
+    public void GenericTypeCase1()
+    {
+        var value = GenericType<string, double, int>.Case1([2.1, 3.2]);
+
+        var json = JsonSerializer.Serialize(value, StandardJsonSerializationWithSourceGenJsonContext.Default.GenericTypeStringDoubleInt32);
+
+        Assert.True(JsonNode.DeepEquals(JsonNode.Parse(json), JsonNode.Parse(@"
+        {
+            ""1"": [2.1, 3.2]
+        }")));
+
+        var deserializedValue = JsonSerializer.Deserialize(json, StandardJsonSerializationWithSourceGenJsonContext.Default.GenericTypeStringDoubleInt32)!;
+
+        Assert.Equal([2.1, 3.2], deserializedValue.AsCase1);
+    }
+
+    [Fact]
+    public void GenericTypeCase2()
+    {
+        var value = GenericType<string, double, int>.Case2(
+            GenericType<int, Dictionary<string, string>, double>.Case2(
+                GenericType<double, Dictionary<int, int>, Dictionary<string, string>>.Case1([
+                    new()
+                    {
+                        [0] = 1,
+                        [2] = 3,
+                    },
+                    new()
+                    {
+                        [4] = 5,
+                    }
+                    ])
+                )
+            );
+
+        var json = JsonSerializer.Serialize(value, StandardJsonSerializationWithSourceGenJsonContext.Default.GenericTypeStringDoubleInt32);
+
+        Assert.True(JsonNode.DeepEquals(JsonNode.Parse(json), JsonNode.Parse(@"
+        {
+            ""2"": {
+                ""2"": {
+                    ""1"": [{
+                        ""0"": 1,
+                        ""2"": 3
+                    }, {
+                        ""4"": 5
+                    }]
+                }
+            }
+        }")));
+
+        var deserializedValue = JsonSerializer.Deserialize(json, StandardJsonSerializationWithSourceGenJsonContext.Default.GenericTypeStringDoubleInt32)!;
+
+        Assert.True(deserializedValue.IsCase2);
+
+        Assert.Collection(deserializedValue.AsCase2.AsCase2.AsCase1, dict1 =>
+        {
+            Assert.Equal(2, dict1.Count);
+            Assert.Equal(1, dict1[0]);
+            Assert.Equal(3, dict1[2]);
+        },
+        dict2 =>
+        {
+            Assert.Single(dict2);
+            Assert.Equal(5, dict2[4]);
+        });
     }
 }
