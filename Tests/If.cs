2742b9dd25719d393998b41b9020b32aa273a97d
Christian Daley <christiandaley@gmail.com>
2025-07-20 12:41:26 -0700

    add type conversion tests

---
+++ Tests/If.cs
@@ -0,0 +1,79 @@
+namespace Tests;
+
+using Dotsum;
+
+public partial class If
+{
+
+    [Case("Case0", typeof(double))]
+    [Case("Case1")]
+    partial class DoubleOrNone
+    {
+
+    }
+
+    [Fact]
+    public void Case0()
+    {
+        var passed = false;
+
+        DoubleOrNone.Case0(2.5).IfCase0(value => passed = value == 2.5);
+        DoubleOrNone.Case0(3.0).IfCase1(() => { passed = false; });
+
+        Assert.True(passed);
+    }
+
+    [Fact]
+    public async Task Case0Async()
+    {
+        var passed = false;
+
+        await DoubleOrNone.Case0(2.5).IfCase0(value =>
+        {
+            passed = value == 2.5;
+
+            return Task.CompletedTask;
+        });
+
+        await DoubleOrNone.Case0(3.0).IfCase1(() => 
+        { 
+            passed = false; 
+
+            return Task.CompletedTask;
+        });
+
+        Assert.True(passed);
+    }
+
+    [Fact]
+    public void Case1()
+    {
+        var passed = false;
+
+        DoubleOrNone.Case1.IfCase1(() => { passed = true; });
+        DoubleOrNone.Case1.IfCase0(_ => { passed = false; });
+
+        Assert.True(passed);
+    }
+
+    public async Task Case1Async()
+    {
+        var passed = false;
+
+        await DoubleOrNone.Case1.IfCase1(() =>
+        {
+            passed = true;
+
+            return Task.CompletedTask;
+        });
+
+        await DoubleOrNone.Case1.IfCase0(value =>
+        {
+            passed = false;
+
+            return Task.CompletedTask;
+        });
+
+        Assert.True(passed);
+    }
+}
---
+++ Tests/ImplicitConversions.cs
@@ -0,0 +1,48 @@
+namespace Tests;
+
+using Dotsum;
+
+public partial class Conversions
+{
+
+    [Case("Case0", typeof(double))]
+    [Case("Case1")]
+    partial class DoubleOrNone
+    {
+
+    }
+
+    [Case("Case0", "T")]
+    [Case("Case1", typeof(int))]
+    [Case("Case2", typeof(string))]
+    [Case("Case3", typeof(double))]
+    [Case("Case4", typeof(int))]
+    [Case("Case5")]
+    partial class GenericType<T>
+    {
+
+    }
+
+    [Fact]
+    public void DoubleOrNoneConversion()
+    {
+        DoubleOrNone d = 5.0;
+
+        Assert.Equal(DoubleOrNone.Case0(5.0), d);
+    }
+
+    [Fact]
+    public void GenericTypeConversion()
+    {
+        GenericType<byte> x1 = 1;
+        Assert.Equal(GenericType<byte>.Case0(1), x1);
+
+        GenericType<byte> x2 = "hello";
+        Assert.Equal(GenericType<byte>.Case2("hello"), x2);
+
+        GenericType<byte> x3 = 3.5;
+        Assert.Equal(GenericType<byte>.Case3(3.5), x3);
+    }
+
+
+}
