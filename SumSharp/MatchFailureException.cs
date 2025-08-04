c816a7f0b5ec7b9d3d199b286ee8c6904e60b795
Christian Daley <christiandaley@gmail.com>
2025-08-04 10:50:41 -0700

    add xml documentation for exception

--- SumSharp/MatchFailureException.cs
+++ SumSharp/MatchFailureException.cs
@@ -2,7 +2,14 @@
 
 namespace SumSharp;
 
+/// <summary>
+/// Thrown when a Match or Switch invocation on a union lacks a handler for the active case
+/// </summary>
+/// <param name="caseName">The name of the active case held by the union</param>
 public sealed class MatchFailureException(string caseName) : Exception($"Match failed to handle case {caseName}")
 {
+    /// <summary>
+    /// The name of the active case held by the union
+    /// </summary>
     public string CaseName => caseName;
 }
