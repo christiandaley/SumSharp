03f08359c5d618af24944e927888b9b96b0cf009
Christian Daley <christiandaley@gmail.com>
2026-07-30 10:13:04 -0700

    update tests again

--- Tests/Equals.cs
+++ Tests/Equals.cs
@@ -100,27 +100,18 @@ public partial class Equals
     {
         Assert.True("abc" == StringOrDoubleExtended.Case0("abc"));
         Assert.True("efg" != StringOrDoubleExtended.Case0("abc"));
+        Assert.True(3.45 != StringOrDoubleExtended.Case0("abc"));
 
         Assert.True(StringOrDoubleExtended.Case0("abc") == "abc");
         Assert.True(StringOrDoubleExtended.Case0("abc") != "efg");
+        Assert.True(StringOrDoubleExtended.Case0("abc") != 3.45);
 
         Assert.True(StringOrDoubleExtended.Case1(3.45) == 3.45);
         Assert.True(StringOrDoubleExtended.Case1(3.45) != 3.46);
+        Assert.True(StringOrDoubleExtended.Case1(3.45) != "abc");
 
         Assert.True(3.45 == StringOrDoubleExtended.Case1(3.45));
         Assert.True(3.46 != StringOrDoubleExtended.Case1(3.45));
-
-
-        Assert.True("abc" == StringOrDoubleExtended.Case2("abc"));
-        Assert.True("efg" != StringOrDoubleExtended.Case2("abc"));
-
-        Assert.True(StringOrDoubleExtended.Case2("abc") == "abc");
-        Assert.True(StringOrDoubleExtended.Case2("abc") != "efg");
-
-        Assert.True(StringOrDoubleExtended.Case3(3.45) == 3.45);
-        Assert.True(StringOrDoubleExtended.Case3(3.45) != 3.46);
-
-        Assert.True(3.45 == StringOrDoubleExtended.Case3(3.45));
-        Assert.True(3.46 != StringOrDoubleExtended.Case3(3.45));
+        Assert.True("abc" != StringOrDoubleExtended.Case1(3.45));
     }
 }
