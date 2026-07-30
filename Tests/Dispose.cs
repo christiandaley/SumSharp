9b91861df04f81740b57834a47e0f2d41f19338f
Christian Daley <christiandaley@gmail.com>
2026-07-30 15:00:01 -0700

    add test for combined dispose and disposeasync

--- Tests/Dispose.cs
+++ Tests/Dispose.cs
@@ -46,6 +46,13 @@ public partial class Dispose
 
     }
 
+    [UnionCase("Case0", typeof(Disposable))]
+    [UnionCase("Case1", typeof(AsyncDisposable))]
+    partial class DisposableOrAsyncDisposable
+    {
+
+    }
+
     [Fact]
     public void NonGenericDispose()
     {
@@ -101,6 +108,60 @@ public partial class Dispose
             await using StringOrAsyncDisposable value = new AsyncDisposable(() => disposed = true);
 
             Assert.False(disposed);
+        }
+
+        Assert.True(disposed);
+    }
+
+    [Fact]
+    public async Task GenericAsyncDispose()
+    {
+        bool disposed = false;
+
+        {
+            await using GenericStringOrDisposable<AsyncDisposable> value = "string";
+        }
+
+        Assert.False(disposed);
+
+        {
+            await using GenericStringOrDisposable<AsyncDisposable> value = new AsyncDisposable(() => disposed = true);
+
+            Assert.False(disposed);
+        }
+
+        Assert.True(disposed);
+    }
+
+    [Fact]
+    public async Task DisposableAndAsyncDisposable()
+    {
+        bool disposed = false;
+
+        {
+            using DisposableOrAsyncDisposable value = new Disposable(() => disposed = true);
+        }
+
+        Assert.True(disposed);
+
+        disposed = false;
+
+        {
+            await using DisposableOrAsyncDisposable value = new Disposable(() => disposed = true);
+        }
+
+        Assert.True(disposed);
+
+        disposed = false;
+
+        {
+            using DisposableOrAsyncDisposable value = new AsyncDisposable(() => disposed = true);
+        }
+
+        Assert.False(disposed);
+
+        {
+            await using DisposableOrAsyncDisposable value = new AsyncDisposable(() => disposed = true);
         }
 
         Assert.True(disposed);
