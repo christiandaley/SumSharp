386e41b2909a30c2ce540a641f869fc02c8df886
Christian Daley <christiandaley@gmail.com>
2026-07-30 15:09:39 -0700

    small change

--- SumSharp.Generator/SymbolHandler.cs
+++ SumSharp.Generator/SymbolHandler.cs
@@ -1445,6 +1445,7 @@ internal class SymbolHandler
     {{
         await DisposeAsyncCore().ConfigureAwait(false);
 
+        {(IsDisposable ? "Dispose(false); " : "")}
         System.GC.SuppressFinalize(this);
     }}
 
