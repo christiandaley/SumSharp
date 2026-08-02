c2441d4b5bc7d1aa94c68f609762410bbcb52dca
Christian Daley <christiandaley@gmail.com>
2026-08-02 08:43:18 -0700

    update test again

--- Tests.Net11/Union.cs
+++ Tests.Net11/Union.cs
@@ -5,12 +5,30 @@ namespace Tests.Net11;
 public partial class Union
 {
     [UnionCase("Int", typeof(int))]
-    [UnionCase("Float", typeof(float))]
+    [UnionCase("String", typeof(string))]
     [UnionCase("Other", "T")]
 
     partial class IntOrStringOrOther<T>
     {
 
+    }
+
+    /*[UnionCase("Case0", typeof(string))]
+    [UnionCase("Case1", typeof(string))]
+    partial class RepeatedCases
+    {
+
+    }*/
+
+    [Fact]
+    public void Value()
+    {
+        IntOrStringOrOther<bool> w = 5;
+
+        Assert.Equal(5, IntOrStringOrOther<bool>.Int(5).Value);
+        Assert.Equal("abc", IntOrStringOrOther<bool>.String("abc").Value);
+        Assert.Equal(true, IntOrStringOrOther<bool>.Other(true).Value);
+        Assert.Equal(4, IntOrStringOrOther<int>.Other(4).Value);
     }
 
 
@@ -21,17 +39,17 @@ public partial class Union
 
         Assert.True(w switch
         {
-            int i => true,
-            float f => false,
+            int i => i == 5,
+            string s => false,
             bool b => false,
         });
 
-        IntOrStringOrOther<bool> x = 3.4f;
+        IntOrStringOrOther<bool> x = "abc";
 
         Assert.True(x switch
         {
             int i => false,
-            float f => true,
+            string s => s == "abc",
             bool b => false,
         });
 
@@ -40,16 +58,16 @@ public partial class Union
         Assert.True(y switch
         {
             int i => false,
-            float f => false,
-            bool b => true,
+            string s => false,
+            bool b => b,
         });
 
         var z = IntOrStringOrOther<int>.Other(4);
 
         Assert.True(z switch
         {
-            int i => true,
-            float f => false,
+            int i => i == 4,
+            string s => false,
         });
     }
 }
