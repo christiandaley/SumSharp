48c2dbf751fe9f277048d55bb158f0cc6745c5c9
Christian Daley <christiandaley@gmail.com>
2026-08-01 19:31:46 -0700

    start work on test

---
+++ Tests.Net11/Union.cs
@@ -0,0 +1,22 @@
+﻿using SumSharp;
+
+namespace Tests.Net11;
+
+public partial class Union
+{
+    [UnionCase("Int", typeof(int))]
+    [UnionCase("Float", typeof(float))]
+    [UnionCase("Other", "T")]
+
+    partial class IntOrStringOrOther<T>
+    {
+
+    }
+
+
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
