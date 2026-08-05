0be624c4667504cf82415d2df44bb7712f2555ed
Christian Daley <christiandaley@gmail.com>
2026-08-04 17:01:01 -0700

    update docs again

--- SumSharp.Generator/SymbolHandler.cs
+++ SumSharp.Generator/SymbolHandler.cs
@@ -785,7 +785,7 @@ internal class SymbolHandler
             }
 
             Builder.AppendLine($@"
-///<summary>Used to implement .NET 11 union requirements for {XMLEscapedName}. Use this type when pattern matching using C#'s built-in switch statement</summary>
+///<summary>Used to implement .NET 11 union requirements. Use this type when pattern matching using C#'s built-in switch statement</summary>
 public partial record struct {caseData.Name};");
         }
 
