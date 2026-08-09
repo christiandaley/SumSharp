cd9bf73603f00b9ec6db40c8763987988885a626
Christian Daley <christiandaley@gmail.com>
2026-08-09 11:51:17 -0700

    fix accessibility of case structs (#32)
    * fix accessibility of case structs
    
    * small change

--- SumSharp.Generator/SymbolHandler.cs
+++ SumSharp.Generator/SymbolHandler.cs
@@ -452,9 +452,7 @@ internal class SymbolHandler
             }
             else
             {
-                var parsedTypeArguments = TypeNameParser.ExtractLeafTypes(caseData.TypeInfo.Name);
-
-                var caseStructTypeArguments = TypeArguments.Intersect(parsedTypeArguments).ToArray();
+                var caseStructTypeArguments = TypeArguments.Intersect(TypeNameParser.ExtractLeafTypes(caseData.TypeInfo.Name)).ToArray();
 
                 var caseStructTypeConstraints =
                     caseStructTypeArguments
@@ -836,14 +834,14 @@ internal class SymbolHandler
             {
                 Builder.AppendLine($@"
     ///<summary>Used to implement .NET 11 union requirements. Use this type when pattern matching using C#'s built-in switch statement</summary>
-    public readonly partial record struct {caseData.Name};");
+    {Accessibility} readonly partial record struct {caseData.Name};");
             }
             else
             {
                 Builder.AppendLine($@"
     ///<summary>Used to implement .NET 11 union requirements. Use this type when pattern matching using C#'s built-in switch statement</summary>
     {GeneratedCodeAttribute}
-    public readonly record struct {Net11StructNameMap[caseData].NameWithTypeArgs}({caseData.TypeInfo.Name} Value) {Net11StructNameMap[caseData].Constraints};");
+    {Accessibility} readonly record struct {Net11StructNameMap[caseData].NameWithTypeArgs}({caseData.TypeInfo.Name} Value) {Net11StructNameMap[caseData].Constraints};");
             }
         }
 
